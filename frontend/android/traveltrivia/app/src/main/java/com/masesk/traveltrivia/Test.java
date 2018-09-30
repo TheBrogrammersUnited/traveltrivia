d95341a7d743d9fc9e50f39f5af274151e023ebc
radon 5k <maseskrikorian@gmail.com>
2018-09-29 18:12:56 -0700

    Added JSON formatter

--- frontend/android/traveltrivia/app/src/main/java/com/masesk/traveltrivia/Test.java
+++ frontend/android/traveltrivia/app/src/main/java/com/masesk/traveltrivia/Test.java
@@ -5,6 +5,9 @@ import android.os.AsyncTask;
 import android.os.Bundle;
 import android.widget.TextView;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 import org.scribe.model.Request;
 import org.scribe.model.Response;
 import org.scribe.model.Verb;
@@ -20,18 +23,38 @@ public class Test extends Activity {
         new OTDBConnect().execute();
     }
 
-    public class OTDBConnect extends AsyncTask<String, String, String> {
+    //skeleton for Open Trivia Database
+
+
+    public class OTDBConnect extends AsyncTask<Void, Void, String> {
         @Override
-        protected String doInBackground(String... strings) {
-            final String URL = "https://opentdb.com/api.php?amount=10";
+        protected String doInBackground(Void... params) {
+            final String URL = "https://opentdb.com/api.php?amount=50";
             Request request = new Request(Verb.GET, URL);
             Response resp = request.send();
+            try {
+                JSONObject jObject = new JSONObject(resp.getBody());
+
+            }catch (JSONException e){
+
+            }
             return resp.getBody();
         }
         @Override
         protected void onPostExecute(String result) {
-            txt = (TextView)findViewById(R.id.textView);
-            txt.setText(result);
+            txt.setText("");
+            StringBuilder builder = new StringBuilder();
+            try {
+                JSONObject jObject = new JSONObject(result);
+                JSONArray jArray = jObject.getJSONArray("results");
+                for(int i = 0; i < jArray.length(); i++) {
+                    JSONObject temp = jArray.getJSONObject(i) ;
+                    builder.append(temp.getString("question") + "\n\n");
+                }
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+            txt.setText(builder.toString());
         }
     }
 }
