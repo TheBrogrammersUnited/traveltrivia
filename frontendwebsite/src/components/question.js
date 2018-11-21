0633bdbbeaf272e7dbf63410b9e27df60456dadf
Sarmena1990 <Sarmena1990@gmail.com>
2018-11-21 11:20:05 -0800

    continue website with Varuj React

--- frontendwebsite/src/components/question.js
+++ frontendwebsite/src/components/question.js
@@ -1,10 +1,18 @@
 import React from 'react';
 
-const Question = ({question}) => {
+const Question = ({question,category,difficulty}) => {
 
 	return(
-		<div  >
-			<div className = 'bg-light-red dib br3 pa3 ma2 grow'>
+		<div className = ''>
+			<div className = 'bg-white helvetica dib br4 ba bw1 pa3 ma2'>
+				Category:  <br/> {category}
+				
+			</div>
+			<div className = 'bg-white dib br4 ba bw1 pa3 ma2'>
+				Difficulty: <br/> {difficulty}
+				
+			</div><br/>
+			<div className = 'bg-white w-third dib br4 ba bw1 pa3 ma2'>
 				{question}
 			</div><br/>
 		</div>
--- frontendwebsite/src/index.js
+++ frontendwebsite/src/index.js
@@ -1,4 +1,3 @@
-
 import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
 
@@ -8,30 +7,19 @@ import 'tachyons';
 //COMPONENTS: no javascript extensions needed but others are
 import Header from './components/header';
 
-// const App = () =>{
-// 	console.log(JSON)
-// 	//return React.createElement(element,object,what is gonna be inside element. It can also be another createElement function with another element passed in);
-// 	//(this will do the same thing as follows) React.createElement('h1', {className: 'title'}, 'Hello, World!');
-// 	return(
-// 			<div>
-// 				<Header/> 
-// 			</div>
-// 		)
-// } 
-
-
 class App extends Component{
+
 	state={
 		news: JSON
 	}
+
+
 	render(){
 		return (
 				<div>
 					<Header/>
-					<NewsList news={this.state.news}>
-					
+						<NewsList news={this.state.news}>
 					</NewsList>
-
 				</div>
 			)
 	}
