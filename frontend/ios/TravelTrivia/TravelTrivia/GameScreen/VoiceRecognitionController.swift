87dfec271881c8ffb2e2e2805d0d8f70092eaadf
seanymac <>
2018-11-26 21:44:36 -0800

    iOS: fixed microphone permissions causing crash on iOS 11+

--- frontend/ios/TravelTrivia/TravelTrivia/GameScreen/VoiceRecognitionController.swift
+++ frontend/ios/TravelTrivia/TravelTrivia/GameScreen/VoiceRecognitionController.swift
@@ -16,10 +16,9 @@ class VoiceRecognitionController
     func Init()
     {
         let lmGenerator = OELanguageModelGenerator()
-        let words = ["silk", "fire", "loyalty", "favorable", "ask", "outfit", "conductor", "sit", "beard", "stream", "physics", "ridge", "decade", "enfix", "deadly", "flavor", "assertive", "circulate", "network", "battery", "economics", "clean", "spot", "consumer", "conference", "likely", "rise", "kettle", "enemy", "firefighter", "deliver", "pig", "sentiment", "jewel", "wheat", "tired", "smash", "demonstrate", "stay", "wrist", "suite", "ballot", "bloody", "worm", "possibility", "counter", "chair", "biography", "housewife", "low", "install", "happen", "peak", "shareholder", "biscuit", "coma", "coin", "owl", "virus", "dead", "even", "demonstrate", "sleeve", "relax", "bolt", "salesperson", "escape", "jail", "abandon", "form", "war", "absorb", "licence", "die", "hallway", "deviation", "dialect", "wriggle", "champagne", "system", "economy", "plain", "flag", "constitution", "government", "deteriorate", "secular", "duck", "nuclear", "ton", "suite", "dream", "grand", "variation", "exclusive", "estate", "relinquish", "brush", "punish", "scholar", "woman", "aviation", "have", "characteristic", "nonsense", "tube", "policeman", "choke", "wrong", "digital", "summer", "fire", "snub", "surround", "choose", "kidney", "fun", "Ayy", "Bee", "See", "Dee"]
+        //let words = ["silk", "fire", "loyalty", "favorable", "ask", "outfit", "conductor", "sit", "beard", "stream", "physics", "ridge", "decade", "enfix", "deadly", "flavor", "assertive", "circulate", "network", "battery", "economics", "clean", "spot", "consumer", "conference", "likely", "rise", "kettle", "enemy", "firefighter", "deliver", "pig", "sentiment", "jewel", "wheat", "tired", "smash", "demonstrate", "wrist", "suite", "ballot", "bloody", "worm", "possibility", "counter", "chair", "biography", "housewife", "low", "install", "happen", "peak", "shareholder", "biscuit", "coma", "coin", "owl", "virus", "dead", "even", "demonstrate", "sleeve", "relax", "bolt", "salesperson", "escape", "jail", "abandon", "fun", "Bbb", "Bb", "Beee", "B", "C", "D", "Bee", "See", "Dee", "Answer", "Repeat", "Repeat the question", "Repeat the answers", "Repeat Question", "Repeat answers", "Stop", "Stop playing", "yes", "no"] // These can be lowercase, uppercase, or mixed-case.
         
-        
-        //["A", "B", "C", "D", "Answer", "Repeat", "Repeat the question", "Repeat the answers", "Repeat Question", "Repeat answers", "Stop", "Stop playing", "yes", "no"] // These can be lowercase, uppercase, or mixed-case.
+        let words = ["Answer A", "Answer B", "Answer Dee", "Answer Be", "Answer C", "Answer See", "Answer D", "Answer Bee", "silk", "fire", "loyalty", "favorable", "ask", "outfit", "conductor",   "stream", "physics", "ridge", "decade", "enfix", "deadly", "flavor", "circulate"]
         let name = "LanguageModelFiles"
         let err: Error! = lmGenerator.generateLanguageModel(from: words, withFilesNamed: name, forAcousticModelAtPath: OEAcousticModel.path(toModel: "AcousticModelEnglish"))
         
@@ -27,14 +26,21 @@ class VoiceRecognitionController
             print("Error while creating initial language model: \(err)")
         } else
         {
-            lmPath = lmGenerator.pathToSuccessfullyGeneratedLanguageModel(withRequestedName: name) // Convenience method to reference the path of a language model known to have been created successfully.
+            lmPath = lmGenerator.pathToSuccessfullyGeneratedLanguageModel(withRequestedName: name) // Convenience method to reference the path of a language model known to have ßbeen created successfully.
             dicPath = lmGenerator.pathToSuccessfullyGeneratedDictionary(withRequestedName: name) // Convenience method to reference the path of a dictionary known to have been created successfully.
         }
+        
+        OEPocketsphinxController.sharedInstance().requestMicPermission();
+        
     }
-
+    
+    func pocketsphinxFailedNoMicPermissions()
+    {
+        print("mic permissions failed");
+    }
     func StartListening()
     {
-        OELogging.startOpenEarsLogging() //Uncomment to receive full OpenEars logging in case of any unexpected results.
+        //OELogging.startOpenEarsLogging() //Uncomment to receive full OpenEars logging in case of any unexpected results.
         do {
             try OEPocketsphinxController.sharedInstance().setActive(true) // Setting the shared OEPocketsphinxController active is necessary before any of its properties are accessed.
         } catch {
